e86ade7957d1f44c4356e8bdd53f45b3fe252e0d
Eureka <1040811211@qq.com>
2019-10-28 19:25:07 +0800

    fix：优化安卓端插件

--- flutter_umeng_plugin/android/src/main/java/com/example/flutter_umeng_plugin/FlutterUmengPlugin.java
+++ flutter_umeng_plugin/android/src/main/java/com/example/flutter_umeng_plugin/FlutterUmengPlugin.java
@@ -1,35 +1,104 @@
 package com.example.flutter_umeng_plugin;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+
+import android.os.Build;
+import android.util.Log;
+import com.umeng.commonsdk.UMConfigure;
+import com.umeng.socialize.PlatformConfig;
+import com.umeng.socialize.ShareAction;
+import com.umeng.socialize.UMAuthListener;
+import com.umeng.socialize.UMShareAPI;
+import com.umeng.socialize.bean.SHARE_MEDIA;
+import com.umeng.socialize.media.UMImage;
+import com.umeng.socialize.media.UMMin;
+import com.umeng.socialize.media.UMVideo;
+import com.umeng.socialize.media.UMWeb;
+import com.umeng.socialize.media.UMusic;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.umeng.socialize.shareboard.ShareBoardConfig;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugin.common.PluginRegistry.ActivityResultListener;
+import io.flutter.plugin.common.PluginRegistry.NewIntentListener;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
+import io.flutter.plugin.common.PluginRegistry.RequestPermissionsResultListener;
 
 /** FlutterUmengPlugin */
-public class FlutterUmengPlugin implements MethodCallHandler {
+public class FlutterUmengPlugin implements MethodCallHandler, ActivityResultListener, RequestPermissionsResultListener  {
+
+    private final Registrar registrar;
+    private final MethodChannel channel;
+
   /** Plugin registration. */
   public static void registerWith(Registrar registrar) {
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "umeng_plugin");
-    channel.setMethodCallHandler(new FlutterUmengPlugin());
+      channel.setMethodCallHandler(new FlutterUmengPlugin(registrar, channel));
+
   }
+
+    private FlutterUmengPlugin(Registrar registrar, MethodChannel channel) {
+        this.registrar = registrar;
+        this.channel = channel;
+    }
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
     if (call.method.equals("shareInit")) {
-      result.success("Android");
+        UMConfigure.setLogEnabled(true);
+        String appkey = call.argument("umengAppkey");
+        String wxAppKey = call.argument("wxAppKey");
+        String wxAppSecret = call.argument("wxAppSecret");
+
+        UMConfigure.init(this.registrar.context(), appkey, "flutter_umeng_plugin", UMConfigure.DEVICE_TYPE_PHONE, "");//58edcfeb310c93091c000be2 5965ee00734be40b580001a0
+        PlatformConfig.setWeixin(wxAppKey, wxAppSecret);
+//        PlatformConfig.setSinaWeibo(appKey, appSecret, redirectURL);
+//        PlatformConfig.setQQZone(appKey, appSecret, redirectURL);
+        result.success("shareInitSuccess");
     } 
     else if (call.method.equals("shareText")) {
-      result.success("Android");
+        String shareText = call.argument("shareString");
+         new ShareAction(registrar.activity()).withText(shareText).setDisplayList(SHARE_MEDIA.SINA,SHARE_MEDIA.QQ,SHARE_MEDIA.WEIXIN)
+                .setCallback(new UmengshareActionListener(registrar.activity(), result)).open();
     } 
     else if (call.method.equals("shareImage")) {
-      result.success("Android");
+        String shareImage = call.argument("shareImage");
+        final Activity activity = registrar.activity();
+        UMImage sImage = new UMImage(activity, shareImage);  //分享图
+        new ShareAction(activity)
+                .setDisplayList(SHARE_MEDIA.SINA,SHARE_MEDIA.QQ,SHARE_MEDIA.WEIXIN)
+                .withMedia(sImage)
+                .setCallback(new UmengshareActionListener(activity, result)).open();
     } 
     else if (call.method.equals("shareImageText")) {
       result.success("Android");
-    } 
+    }
     else if (call.method.equals("shareWebView")) {
-      result.success("Android");
+        String shareTitle = call.argument("shareTitle");
+        String descr = call.argument("descr");
+        String icon = call.argument("icon");
+        String webUrl = call.argument("webUrl");
+        Activity activity = registrar.activity();
+        UMImage thumbImage = new UMImage(activity, icon);
+        UMWeb web = new UMWeb(webUrl);
+        web.setTitle(shareTitle);//标题
+        web.setThumb(thumbImage);  //缩略图
+        web.setDescription(descr);//描述
+        new ShareAction(activity).setDisplayList(SHARE_MEDIA.SINA,SHARE_MEDIA.QQ,SHARE_MEDIA.WEIXIN)
+                .withMedia(web)
+                .setCallback(new UmengshareActionListener(activity, result)).open();
     } 
     else if (call.method.equals("shareMusic")) {
       result.success("Android");
@@ -44,15 +113,15 @@ public class FlutterUmengPlugin implements MethodCallHandler {
       result.success("Android");
     } 
      else if (call.method.equals("loginWX")) {
-      result.success("Android");
-    } 
+        login(SHARE_MEDIA.WEIXIN,result);
+    }
      else if (call.method.equals("loginQQ")) {
-      result.success("Android");
+        login(SHARE_MEDIA.QQ,result);
     } 
      else if (call.method.equals("loginSina")) {
-      result.success("Android");
+        login(SHARE_MEDIA.SINA,result);
     } 
-       else if (call.method.equals("analyticsInit")) {
+     else if (call.method.equals("analyticsInit")) {
       result.success("Android");
     } 
        else if (call.method.equals("beginPageView")) {
@@ -64,14 +133,56 @@ public class FlutterUmengPlugin implements MethodCallHandler {
       else if (call.method.equals("logPageView")) {
       result.success("Android");
     } 
-          else if (call.method.equals("analyticsEvent")) {
+     else if (call.method.equals("analyticsEvent")) {
       result.success("Android");
-    } 
-          else if (call.method.equals("configure")) {
+    }
+     else if (call.method.equals("configure")) {
       result.success("Android");
     } 
     else {
       result.notImplemented();
     }
   }
+
+    private void login(SHARE_MEDIA platform, final Result result) {
+
+        UMShareAPI.get(registrar.activity()).getPlatformInfo(registrar.activity(), platform, new UMAuthListener() {
+            @Override
+            public void onStart(SHARE_MEDIA share_media) {
+
+            }
+            @Override
+            public void onComplete(SHARE_MEDIA share_media, int i, Map<String, String> map) {
+                map.put("status", "success");
+                result.success(map);
+
+            }
+
+            @Override
+            public void onError(SHARE_MEDIA share_media, int i, Throwable throwable) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("status", "fail");
+                result.success(map);
+            }
+            @Override
+            public void onCancel(SHARE_MEDIA share_media, int i) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("status", "fail");
+                result.success(map);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onActivityResult(int i, int i1, Intent intent) {
+        UMShareAPI.get(registrar.activity()).onActivityResult(i, i1, intent);
+        return false;
+    }
+
+    @Override
+    public boolean onRequestPermissionsResult(int i, String[] strings, int[] ints) {
+        return false;
+    }
+
 }
---
+++ flutter_umeng_plugin/android/src/main/java/com/example/flutter_umeng_plugin/wxapi/WXEntryActivity.java
@@ -0,0 +1,6 @@
+package com.example.flutter_umeng_plugin.wxapi;
+
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+public class WXEntryActivity extends WXCallbackActivity {
+}
